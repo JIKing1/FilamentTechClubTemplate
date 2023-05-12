8aa73c798725274d5d219a757d6f64553f531eab
JIKing1 <133062554+JIKing1@users.noreply.github.com>
2023-05-12 15:54:15 -0700

    Button fully works!

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,27 @@
 
 function changeColor() {
-    document.getElementById("JamesRocks").style.color = "orange";
+   var currentColor = document.getElementById("JamesRocks").style.color;
+   var newColor = selectNewColor(currentColor);
+    document.getElementById("JamesRocks").style.color = newColor;
+
+}
+
+
+function selectNewColor(currentColor) {
+    var newColor = "";
+
+    if (currentColor === "") {
+        newColor = "purple";
+    } else if (currentColor === "purple"){
+        newColor = "red";
+    } else if (currentColor === "red") {
+        newColor = "olive";
+    } else if (currentColor === "olive") {
+        newColor = "";
+    }
+
+
+    return newColor;
+
+
 }
